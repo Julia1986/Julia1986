0c528c4864e854fc142ed93ec2c1fad025443cab
Julia1986 <59840160+Julia1986@users.noreply.github.com>
2020-03-11 08:24:39 +0200

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,19 @@
+$('#carousel1').owlCarousel({
+    loop:true,
+    margin:10,
+	center: true,
+    nav:true,
+	dots: false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
